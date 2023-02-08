d45bbb3c62770f3ae5206507a64a5526ce1a230c
TommasoBernardo <tommasob1210@gmail.com>
2023-02-08 19:08:40 +0100

    update

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>@yield('title', 'Comics | DC')</title>
+    <link rel="icon" href="{{ Vite::asset('resources/assets/img/favicon.ico') }}" type="image/icon type">
+    <!-- Styles -->
+    @vite('resources/js/app.js')
+    {{-- Font-Awesome --}}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css"
+        integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+</head>
+
+<body>
+    @include('partials.header')
+    <main>
+        @yield('main-content')
+    </main>
+    @include('partials.footer')
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    $cards = config('comics.card');
+    return view('comics', ['cards' => $cards]);
+})->name('comics');
